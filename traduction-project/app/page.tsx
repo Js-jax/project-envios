0c98928419974ca2e55d280125d5853abd87005e
Josue Hernández <josue.hernandez@geekinc.mx>
2023-08-17 02:44:43 -0600

    Continuando desarrollo de HomePage

--- traduction-project/app/page.tsx
+++ traduction-project/app/page.tsx
@@ -1,32 +1,51 @@
-import { ChevronRightIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image'
-import assist1 from '@/assets/asistente.webp';
-import assist2 from '@/assets/asistente1.webp';
-import assist3 from '@/assets/asistente2.webp';
-import assist4 from '@/assets/asistente3.webp';
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import assist1 from "@/assets/asistente.webp";
+import assist2 from "@/assets/asistente1.webp";
+import assist3 from "@/assets/asistente2.webp";
+import assist4 from "@/assets/asistente3.webp";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:pt-24 lg:px-8">
       <div className="relative isolate pt-14">
-          
-          <div className="mx-auto max-w-7xl lg:flex lg:items-center lg:gap-x-10 lg:px-8">
-            <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
-              <h1 className="max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                A better way to ship your projects
-              </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Esse id magna consectetur fugiat non dolor in ad laboris magna laborum ea consequat. Nisi irure aliquip
-                nisi adipisicing veniam voluptate id. In veniam incididunt ex veniam adipisicing sit.
-              </p>
-              
-            </div>
-            <div className="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
-              {/* <Image/> */}
-              <Image src={assist1} alt='Asistente 1' className='w-52 border rounded-lg mx-auto'/>
-            </div>
+        <div className="mx-auto max-w-7xl lg:flex lg:items-center lg:gap-x-10 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
+            <h1 className="max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Ayuda profesional
+            </h1>
+            <p className="mt-6 text-xl leading-8 text-gray-600">
+              Obtenga la ayuda que necesita para liberar el potencial de su
+              firma, apóyese en profesionales que le brindará soporte en su
+              operación.
+            </p>
+            <p className="mt-6 text-md text-gray-400">
+              Del Campo esta aqui para apoyarte con el talento, comienza con tu
+              asistente hoy.
+            </p>
+          </div>
+          <div className="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
+            {/* <Image/> */}
+            <Image
+              src={assist1}
+              alt="Asistente 1"
+              className="w-52 border rounded-lg mx-auto"
+            />
           </div>
         </div>
+      </div>
+      <div className="relative isolate pt-14">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-6xl text-center">
+            Estas son algunas tareas que puede realizar su asistente legal
+            virtual
+          </h2>
+        </div>
+        <div className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
